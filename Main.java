e7cd9eb8fe7d0b0b2bd6c7ac479fbc955837bed8
Rahul Dalvi <rahuldalvi246@gmail.com>
2022-11-08 17:57:00 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+// import java.util.Scanner;
+// public class Main{
+//     public static void fib(int n){
+//         int a=0;
+//         int b=1;
+//         for(int i=0;i<n;i++){
+//             System.out.print(a+" ");
+//             int c=a+b;
+//             a=b;
+//             b=c;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         fib(n);
+
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class Main{
+//     public static int fib(int n){
+//         if(n==0 || n==1){
+//             return n;
+//         }
+//         return fib(n-1)+fib(n-2);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=0;i<n;i++){
+//             System.out.print(fib(i)+" ");
+//         }
+
+//     }
+// }
